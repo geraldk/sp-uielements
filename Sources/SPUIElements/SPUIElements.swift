a8bd5ff5198ad8ca3afa27ef5211d33ad2f6df67
Gerald <geraldk675@gmail.com>
2021-03-08 18:11:23 +1100

    sdfsda

--- Sources/SPUIElements/SPUIElements.swift
+++ Sources/SPUIElements/SPUIElements.swift
@@ -1,3 +1,11 @@
+import UIKit
+
 struct SPUIElements {
     var text = "Hello, World!"
+}
+
+public struct UIElements {
+    public let tableCellNib = UINib(nibName: "TableCell", bundle: .module)
+    
+    public init() {}
 }
